b5c195981b0a365050420a661eefc31676343f98
CarterFluckiger <54374038+CarterFluckiger@users.noreply.github.com>
2019-08-22 14:14:15 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ Carter Fluckiger
+ C++ Fall 2019
+ Due: Due Date
+ Lab 1 Introduction to C++ Programming
+ The lab was a test of downloading other code and uploading my own code to be share on Github which is an essential skill in this class.
+
+*/
+
+ #include <iostream>
+
+
+
+int main()
+{
+	std::cout << "Hello World! My favorite color is Blue";
+	return 0;
+}
+
+
