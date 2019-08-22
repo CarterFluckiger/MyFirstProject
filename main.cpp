bb6715899bc75ac487dd13d71b538ed8d3aaaa3b
CarterFluckiger <54374038+CarterFluckiger@users.noreply.github.com>
2019-08-22 14:16:04 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  Carter Fluckiger
  C++ Fall 2019
- Due: Due Date
+ Due: August 29, 2019
  Lab 1 Introduction to C++ Programming
  The lab was a test of downloading other code and uploading my own code to be share on Github which is an essential skill in this class.
 
